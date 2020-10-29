b3b9c70cd13a0b093438433e61bcae4dead30068
hasali-coder <58490554+hasali-coder@users.noreply.github.com>
2020-10-30 01:27:13 +0300

    added remote mysql connection details

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,7 +1,8 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "maskani");
+$con=mysqli_connect("remotemysql.com", "QIvVP5K1Jl", "vgMfRqh7rK", "QIvVP5K1Jl");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
 
   ?>
+
